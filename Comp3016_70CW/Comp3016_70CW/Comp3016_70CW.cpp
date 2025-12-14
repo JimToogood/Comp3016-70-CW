6b916cf83f0acaeca88c083295501d7c3abac0a4
Jim Toogood <jimtoogood04@gmail.com>
2025-12-14 13:35:25 +0000

    Added Controllable Camera and Textures
    - Added camera, controllable with WASD/Space/LShift/Mouse, to explore and observe 3D environment
    - Added texture supports for vertices
    - Cleaned up unnecessary/untidy comments and code snippets

--- Comp3016_70CW/Comp3016_70CW/Comp3016_70CW.cpp
+++ Comp3016_70CW/Comp3016_70CW/Comp3016_70CW.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
+#include <vector>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+#include "glm/glm/ext/vector_float3.hpp"
+#include <glm/glm/ext/matrix_transform.hpp>
+#include <glm/glm/gtc/type_ptr.hpp>
+
 #include "main.h"
 #include "LoadShaders.h"
+
 using namespace std;
+using namespace glm;
 
 
 // VAO vertex attribute positions in correspondence to vertex attribute type
@@ -14,8 +24,25 @@ GLuint VAOs[NumVAOs];       // VAOs
 enum Buffer_IDs { ArrayBuffer, NumBuffers = 4 };
 GLuint Buffers[NumBuffers];     // Buffer objects
 
-// Define shaders program
-GLuint program;
+// -=-=- Window -=-=-
+int windowWidth = 1280;
+int windowHeight = 720;
+
+// -=-=- Camera -=-=-
+vec3 cameraPosition = vec3(0.0f, 0.0f, 3.0f);
+vec3 cameraFront = vec3(0.0f, 0.0f, -1.0f);
+vec3 cameraUp = vec3(0.0f, 1.0f, 0.0f);
+
+float cameraYaw = -90.0f;   // Sideways rotation
+float cameraPitch = 0.0f;   // Vertical rotation
+
+float cameraLastXPos = windowWidth / 2.0f;  // Position of camera from previous frame
+float cameraLastYPos = windowHeight / 2.0f;
+bool mouseFirstEntry = true;
+
+// -=-=- Time -=-=-
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;     // Previous value of time change
 
 
 void FramebufferSizeCallback(GLFWwindow* window, int width, int height) {
@@ -24,32 +51,69 @@ void FramebufferSizeCallback(GLFWwindow* window, int width, int height) {
 }
 
 void ProcessUserInput(GLFWwindow* WindowIn) {
-    // If Escape pressed
+    // Close window on escape pressed
     if (glfwGetKey(WindowIn, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
-        // Tell window to close
         glfwSetWindowShouldClose(WindowIn, true);
     }
-    // If E pressed
-    else if (glfwGetKey(WindowIn, GLFW_KEY_E) == GLFW_PRESS) {
-        cout << "E pressed" << endl;
+
+    const float speed = 2.0f * deltaTime;
+
+    // Camera movement controls
+    if (glfwGetKey(WindowIn, GLFW_KEY_W) == GLFW_PRESS)
+        cameraPosition += speed * cameraFront;
+    if (glfwGetKey(WindowIn, GLFW_KEY_S) == GLFW_PRESS)
+        cameraPosition -= speed * cameraFront;
+    if (glfwGetKey(WindowIn, GLFW_KEY_A) == GLFW_PRESS)
+        cameraPosition -= normalize(cross(cameraFront, cameraUp)) * speed;
+    if (glfwGetKey(WindowIn, GLFW_KEY_D) == GLFW_PRESS)
+        cameraPosition += normalize(cross(cameraFront, cameraUp)) * speed;
+    if (glfwGetKey(WindowIn, GLFW_KEY_SPACE) == GLFW_PRESS)
+        cameraPosition += speed * cameraUp;
+    if (glfwGetKey(WindowIn, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS)
+        cameraPosition -= speed * cameraUp;
+}
+
+void MouseCallback(GLFWwindow* window, double xpos, double ypos) {
+    // Initialise last positions
+    if (mouseFirstEntry) {
+        cameraLastXPos = xpos;
+        cameraLastYPos = ypos;
+        mouseFirstEntry = false;
     }
+
+    float xOffset = xpos - cameraLastXPos;
+    float yOffset = cameraLastYPos - ypos;
+    cameraLastXPos = xpos;
+    cameraLastYPos = ypos;
+
+    // Define mouse sensitivity
+    const float sensitivity = 0.04f;
+    xOffset *= sensitivity;
+    yOffset *= sensitivity;
+
+    cameraYaw += xOffset;
+    cameraPitch += yOffset;
+
+    // Clamp pitch to avoid turning up & down beyond 90 degrees
+    cameraPitch = clamp(cameraPitch, -89.0f, 89.0f);
+
+    // Calculate camera direction
+    vec3 direction;
+    direction.x = cos(radians(cameraYaw)) * cos(radians(cameraPitch));
+    direction.y = sin(radians(cameraPitch));
+    direction.z = sin(radians(cameraYaw)) * cos(radians(cameraPitch));
+    cameraFront = normalize(direction);
 }
 
 
 int main() {
-    // Define variables
-    const int WIN_WIDTH = 800;
-    const int WIN_HEIGHT = 600;
-
     // Initialise GLFW
     glfwInit();
 
-    // Initialise GLFWwindow object
-    GLFWwindow* window = glfwCreateWindow(WIN_WIDTH, WIN_HEIGHT, "test window", NULL, NULL);
-
-    // Check if window has been successfully instantiated
-    if (window == NULL) {
-        cerr << "GLFW Window did not instantiate, exiting..." << endl;
+    // Create window
+    GLFWwindow* window = glfwCreateWindow(windowWidth, windowHeight, "window", NULL, NULL);
+    if (!window) {
+        cerr << "GLFW Window failed to initialise, exiting..." << endl;
         glfwTerminate();
         return -1;
     }
@@ -57,7 +121,12 @@ int main() {
     // Bind OpenGL to window
     glfwMakeContextCurrent(window);
     glewInit();
-    glViewport(0, 0, WIN_WIDTH, WIN_HEIGHT);
+    glViewport(0, 0, 1280, 720);
+
+    // Assign callbacks
+    glfwSetFramebufferSizeCallback(window, FramebufferSizeCallback);    // Use the FramebufferSizeCallback function when window is resized
+    glfwSetCursorPosCallback(window, MouseCallback);                    // Use the MouseCallback function for the mouse movement
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);        // Automatically binds cursor to window & hides pointer
 
     // Load shaders
     ShaderInfo shaders[] = {
@@ -65,19 +134,15 @@ int main() {
         { GL_FRAGMENT_SHADER, "shaders/fragmentShader.frag" },
         { GL_NONE, NULL }
     };
-
     program = LoadShaders(shaders);
     glUseProgram(program);
 
-    // Use the FramebufferSizeCallback function when window is resized
-    glfwSetFramebufferSizeCallback(window, FramebufferSizeCallback);
-
     float vertices[] = {
-        // positions             // colours
-        0.5f, 0.5f, 0.0f,       1.0f, 0.0f, 0.0f,   // top right
-        0.5f, -0.5f, 0.0f,      0.0f, 1.0f, 0.0f,   // bottom right
-        -0.5f, -0.5f, 0.0f,     0.0f, 0.0f, 1.0f,   // bottom left
-        -0.5f, 0.5f, 0.0f,      1.0f, 1.0f, 1.0f    // top left
+        // positions            // textures
+        0.5f, 0.5f, 0.5f,       1.0f, 1.0f,     // top right
+        0.5f, -0.5f, 0.5f,      1.0f, 0.0f,     // bottom right
+        -0.5f, -0.5f, 0.5f,     0.0f, 0.0f,     // bottom left
+        -0.5f, 0.5f, 0.5f,      0.0f, 1.0f      // top left
     };
 
     unsigned int indices[] = {
@@ -102,11 +167,11 @@ int main() {
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     // Allocation & indexing of vertex attribute memory for vertex shader
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
     // Colours
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
 
     // Unbinding
@@ -114,25 +179,85 @@ int main() {
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
-    // Program loop
+    // Texture index
+    unsigned int texture;
+    // Textures to generate
+    glGenTextures(1, &texture);
+
+    // Binding texture to type 2D texture
+    glBindTexture(GL_TEXTURE_2D, texture);
+
+    // Sets to use linear interpolation between adjacent mipmaps
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    // Sets to use linear interpolation upscaling (past largest mipmap texture)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    // Parameters that will be sent & set based on retrieved texture
+    int width, height, colourChannels;
+    // Retrieves texture data
+    unsigned char* data = stbi_load("media/wood.jpg", &width, &height, &colourChannels, 0);
+
+    // If retrieval successful
+    if (data) {
+        // Generation of texture from retrieved texture data
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        // Automatically generates all required mipmaps on bound texture
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    // If retrieval unsuccessful
+    else {
+        cout << "Failed to load texture.\n";
+        return -1;
+    }
+
+    // Clears retrieved texture from memory
+    stbi_image_free(data);
+
+    // Model matrix
+    mat4 model = mat4(1.0f);
+    // Scaling to zoom in
+    model = scale(model, vec3(2.0f, 2.0f, 2.0f));
+    // Rotation to look down
+    model = rotate(model, radians(-45.0f), vec3(1.0f, 0.0f, 0.0f));
+    // Movement to position further back
+    model = translate(model, vec3(0.0f, 1.f, -1.5f));
+
+    // Projection matrix
+    mat4 projection = perspective(radians(45.0f), (float)windowWidth / (float)windowHeight, 0.1f, 100.0f);
+
+    // Game loop
     while (!glfwWindowShouldClose(window)) {
+        // Time management
+        float currentFrame = (float)glfwGetTime();
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+
         // User Input
         ProcessUserInput(window);
 
         // Rendering
-        glClearColor(1.0f, 0.0f, 0.0f, 1.0f);   // RGBA Colour (normalised between 0.0f-1.0f instead of 0-255)
-        glClear(GL_COLOR_BUFFER_BIT);           // Clear the colour buffer
+        glClearColor(0.7f, 0.7f, 0.7f, 1.0f);   // RGBA Colour (normalised between 0.0f-1.0f instead of 0-255)
+        glClear(GL_COLOR_BUFFER_BIT);           // Clear colour buffer
 
-        glBindVertexArray(VAOs[0]);             // Bind buffer object to render
+        // Transformations
+        mat4 view = lookAt(cameraPosition, cameraPosition + cameraFront, cameraUp);     // Sets the position of the viewer, the movement direction in relation to it & the world up direction
+        mat4 mvp = projection * view * model;
+        int mvpLoc = glGetUniformLocation(program, "mvpIn");
+
+        glUniformMatrix4fv(mvpLoc, 1, GL_FALSE, value_ptr(mvp));
+
+        // Drawing
+        glBindTexture(GL_TEXTURE_2D, texture);
+        glBindVertexArray(VAOs[0]);         // Bind buffer object to render;
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         // Refreshing
-        glfwSwapBuffers(window);    // Swap the colour buffer
-        glfwPollEvents();           // Query all GLFW events
+        glfwSwapBuffers(window);    // Swaps the colour buffer
+        glfwPollEvents();           // Queries all GLFW events
     }
 
     // Terminate GLFW
     glfwTerminate();
-    cout << "Program finished without error." << endl;
+    cout << "Program exited." << endl;
     return 0;
 }
--- Comp3016_70CW/Comp3016_70CW/main.h
+++ Comp3016_70CW/Comp3016_70CW/main.h
@@ -4,5 +4,11 @@
 // Window resize function
 void FramebufferSizeCallback(GLFWwindow* window, int width, int height);
 
+// Mouse movement function
+void MouseCallback(GLFWwindow* window, double xpos, double ypos);
+
 // Manages inputs from user
 void ProcessUserInput(GLFWwindow* WindowIn);
+
+// Define shaders program
+GLuint program;
